1877afb7a4d79064afaeda55a6780f8052569e08
Mr-Emerald-Wolf <shivamsharma2002@gmail.com>
2023-06-08 03:13:25 +0530

    fix: image url added to models

--- models/blog.model.go
+++ models/blog.model.go
@@ -6,6 +6,7 @@ type Blog struct {
 	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
 	Title       string    `gorm:"varchar(255);not null" json:"title"`
 	SubTitle    string    `gorm:"varchar(255);not null" json:"subTitle"`
+	Image       string    `gorm:"varchar(255);not null" json:"image"`
 	Description string    `gorm:"varchar(255);not null" json:"description"`
 	Author      string    `gorm:"varchar(255);not null" json:"author"`
 	Date        string    `json:"date"`
@@ -15,6 +16,7 @@ type CreateBlogSchema struct {
 	Title       string `json:"title" validate:"required"`
 	SubTitle    string `json:"subTitle" validate:"required"`
 	Description string `json:"description" validate:"required"`
+	Image       string `json:"image"`
 	Author      string `json:"author" validate:"required"`
 	Date        string `json:"date" validate:"required"`
 }
@@ -23,6 +25,7 @@ type UpdateBlogSchema struct {
 	Title       string `json:"title"`
 	SubTitle    string `json:"subTitle"`
 	Description string `json:"description"`
+	Image       string `json:"image"`
 	Author      string `json:"author"`
 	Date        string `json:"date"`
 }
